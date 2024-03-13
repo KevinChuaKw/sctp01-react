c158fdd115f518f23716aca55fdacdffc8a0b9ca
Kevin Chua <Kevin_chua3@hotmail.com>
2024-03-13 11:33:54 +0000

    Setting up base for in class examples

---
+++ 07-to-do-list/src/App.js
@@ -0,0 +1,13 @@
+import "bootstap/dist/css/bootstrap.min.css"; 
+import "./Tasklist"; 
+import Tasklist from "./Tasklist";
+
+function App() {
+  return (
+    <div className="container">
+        <Tasklist/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 07-to-do-list/src/Tasklist.js
@@ -0,0 +1,5 @@
+import {useState} from "react"; 
+
+export default function (){
+    const [tasks, setTasks]= useState
+}
